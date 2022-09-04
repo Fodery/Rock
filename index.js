8b090a8a0455e3453482398e6e3513bfd74c2c22
Fodery <mmfq8@hotmail.com>
2022-09-04 20:10:29 +0300

    new codes

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const playerText = document.querySelector("#playert");
 const compText = document.querySelector("#compt");
 const resultText = document.querySelector("#resultt");
+const choicebtn = document.querySelectorAll(".choicebtn");
 
+let player;
+let computer;
+let result;
+
+choicebtn.forEach(button => button.addEventListener("click", () => {
+
+player = button.textContent;
+
+} ));
